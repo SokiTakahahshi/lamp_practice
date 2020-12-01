2b6471c77508f0d26edb9c435fb1a64de61e3046
タカハシソウキ <souki1039@gmail.com>
2020-12-02 00:24:22 +0900

    SQLインジェクション

--- www/model/item.php
+++ www/model/item.php
@@ -16,10 +16,10 @@ function get_item($db, $item_id){
     FROM
       items
     WHERE
-      item_id = {$item_id}
+      item_id = ?
   ";
-
-  return fetch_query($db, $sql);
+  $params = array($item_id);
+  return fetch_query($db, $sql,$params);
 }
 
 function get_items($db, $is_open = false){
@@ -39,7 +39,6 @@ function get_items($db, $is_open = false){
       WHERE status = 1
     ';
   }
-
   return fetch_all_query($db, $sql);
 }
 
@@ -82,10 +81,10 @@ function insert_item($db, $name, $price, $stock, $filename, $status){
         image,
         status
       )
-    VALUES('{$name}', {$price}, {$stock}, '{$filename}', {$status_value});
+    VALUES(?,?,?,?,?);
   ";
-
-  return execute_query($db, $sql);
+  $params = array($name,$price,$stock,$filename,$status_value);
+  return execute_query($db, $sql,$params);
 }
 
 function update_item_status($db, $item_id, $status){
@@ -93,13 +92,13 @@ function update_item_status($db, $item_id, $status){
     UPDATE
       items
     SET
-      status = {$status}
+      status = ?
     WHERE
-      item_id = {$item_id}
+      item_id = ?
     LIMIT 1
   ";
-  
-  return execute_query($db, $sql);
+  $params = array($status,$item_id);
+  return execute_query($db, $sql,$params);
 }
 
 function update_item_stock($db, $item_id, $stock){
@@ -107,13 +106,13 @@ function update_item_stock($db, $item_id, $stock){
     UPDATE
       items
     SET
-      stock = {$stock}
+      stock = ?
     WHERE
-      item_id = {$item_id}
+      item_id = ?
     LIMIT 1
   ";
-  
-  return execute_query($db, $sql);
+  $params = array($stock,$item_id);
+  return execute_query($db, $sql,$params);
 }
 
 function destroy_item($db, $item_id){
@@ -136,11 +135,11 @@ function delete_item($db, $item_id){
     DELETE FROM
       items
     WHERE
-      item_id = {$item_id}
+      item_id = ?
     LIMIT 1
   ";
-  
-  return execute_query($db, $sql);
+  $params = array($item_id);
+  return execute_query($db, $sql,$item_id);
 }
 
 
